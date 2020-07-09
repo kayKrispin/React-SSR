71c3af11e025418148e8b108444b2d671f197f55
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2020-07-09 16:26:57 +0300

    a

--- react-ssr/server/index.js
+++ react-ssr/server/index.js
@@ -1,19 +1,22 @@
-const express = require("express");
-const app = express();
+import express from "express";
+import path from "path";
+import React from "react";
+import renderer from "./middleware/renderer";
 
+const app = express();
 const router = express.Router();
 
 const PORT = "8080";
 
-// Home route
-app.get("/", (req, res) => {
-  res.send("Welcome to a baserfrefeferreefeic express App");
-});
+router.use(express.static(
+  path.resolve(__dirname, "..", "build"),
+  { maxAge: "30d", index: false },
+));
 
+router.use('*', renderer);
 
 app.use(router);
 
 app.listen(PORT, () => console.log(`Running on port, ${PORT}`));
-
 
 
