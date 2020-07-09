c5a8f996eda8505166280f182ca8fc2edae394f6
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2020-07-09 16:44:24 +0300

    d

--- react-ssr/index.js
+++ react-ssr/index.js
@@ -1,22 +1,16 @@
-import express from "express";
-import path from "path";
-import React from "react";
-import renderer from "./server/middleware/renderer";
-
-const app = express();
-const router = express.Router();
-
-const PORT = "8080";
-
-router.use(express.static(
-  path.resolve(__dirname, "..", "build"),
-  { maxAge: "30d", index: false },
-));
-
-router.use('*', renderer);
-
-app.use(router);
-
-app.listen(PORT, () => console.log(`Running on port, ${PORT}`));
+require("ignore-styles");
+const fetch = require("node-fetch");
 
 
+require("@babel/register") ({
+  ignore: [/(node_modules)/],
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+    "@babel/plugin-transform-runtime"
+  ]
+});
+
+console.error = () => {};
+
+require("./server/index");
+global.fetch = fetch;
