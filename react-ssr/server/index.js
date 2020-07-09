b676b40baae8a311e7732bf2894d9bc281a08c9a
Taras Bashuryn <tarasbashuryn@Tarass-iMac.local>
2020-07-09 16:24:00 +0300

    d

--- react-ssr/server/index.js
+++ react-ssr/server/index.js
@@ -1,9 +1,6 @@
-import express from "express";
-import path from "path";
-import React from "react";
-import renderer from "./middleware/renderer";
-
+const express = require("express");
 const app = express();
+
 const router = express.Router();
 
 const PORT = "8080";
